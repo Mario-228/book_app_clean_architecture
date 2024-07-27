7762963345216991e0e21ca53d888f9d23d01a4b
MarioYoussef <marioyoussef08@gmail.com>
2024-07-27 19:32:20 +0300

    add either class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,7 @@
 import 'package:book_app_clean_architecture/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Error, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Error, List<BookEntity>>> fetchNewestBooks();
 }
