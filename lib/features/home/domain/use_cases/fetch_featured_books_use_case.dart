7a146cbf2bf0e29d86900ac32ac0c8a5c04ab465
MarioYoussef <marioyoussef08@gmail.com>
2024-07-29 17:45:29 +0300

    create use case class

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,12 +1,14 @@
+import 'package:book_app_clean_architecture/core/use_cases/use_case.dart';
 import 'package:book_app_clean_architecture/features/home/domain/entities/book_entity.dart';
 import 'package:book_app_clean_architecture/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   const FetchFeaturedBooksUseCase({required this.homeRepo});
   final HomeRepo homeRepo;
 
-  Future<Either<Error, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Error, List<BookEntity>>> call({NoParam? param}) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
