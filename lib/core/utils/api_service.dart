d0c9a9b605604111fbbd923f1649e1794a8b4124
MarioYoussef <marioyoussef08@gmail.com>
2024-07-31 15:03:31 +0300

    change the data members

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class ApiService {
   final Dio _dio;
   // final String baseUrl = 'https://api.itbook.store/1.0';
-  final String baseUrl = 'https://www.googleapis.com/books/v1/';
+  static const String baseUrl = 'https://www.googleapis.com/books/v1/';
   ApiService(this._dio);
 
   Future<Map<String, dynamic>> get(String endPoint) async {
