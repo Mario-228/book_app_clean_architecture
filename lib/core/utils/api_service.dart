93880ac815eac4c55882dcaa1ac9c771eca2cdd4
MarioYoussef <marioyoussef08@gmail.com>
2024-07-31 14:46:24 +0300

    create api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio;
+  // final String baseUrl = 'https://api.itbook.store/1.0';
+  final String baseUrl = 'https://www.googleapis.com/books/v1/';
+  ApiService(this._dio);
+
+  Future<Map<String, dynamic>> get(String endPoint) async {
+    var result = await _dio.get(baseUrl + endPoint);
+    return result.data;
+  }
+}
