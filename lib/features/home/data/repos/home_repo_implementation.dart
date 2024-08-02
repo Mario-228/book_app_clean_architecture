e78eaef1d521e23cbd1290f4f7e216ba820b109a
MarioYoussef <marioyoussef08@gmail.com>
2024-08-02 14:37:37 +0300

    create home repo implementation

---
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -0,0 +1,39 @@
+import 'package:book_app_clean_architecture/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:book_app_clean_architecture/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:book_app_clean_architecture/features/home/domain/entities/book_entity.dart';
+import 'package:book_app_clean_architecture/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplementation implements HomeRepo {
+  final HomeLocalDataSource homeLocalDataSource;
+  final HomeRemoteDataSource homeRemoteDataSource;
+  HomeRepoImplementation(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
+  @override
+  Future<Either<Error, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      var booksList = homeLocalDataSource.fetchFeaturedBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      }
+      var data = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(data);
+    } catch (e) {
+      return left(Error());
+    }
+  }
+
+  @override
+  Future<Either<Error, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      var booksList = homeLocalDataSource.fetchNewestBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      }
+      var data = await homeRemoteDataSource.fetchNewestBooks();
+      return right(data);
+    } catch (e) {
+      return left(Error());
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,11 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
-  runApp(const Bookly());
-
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
-  await Hive.openBox(kFeaturedBox);
-  await Hive.openBox(kNewestBox);
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
+  runApp(const Bookly());
 }
 
 class Bookly extends StatelessWidget {
