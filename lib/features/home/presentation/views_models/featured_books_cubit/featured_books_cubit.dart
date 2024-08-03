024eb5ecfced7438e37a0be07e4a817cfa97f21e
MarioYoussef <marioyoussef08@gmail.com>
2024-08-03 18:25:40 +0300

    implement cubit fetch featured books

--- lib/features/home/presentation/views_models/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/views_models/featured_books_cubit/featured_books_cubit.dart
@@ -1,8 +1,22 @@
+import 'package:book_app_clean_architecture/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:book_app_clean_architecture/features/home/presentation/views_models/featured_books_cubit/featured_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitialState());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitialState());
 
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
   static FeaturedBooksCubit get(context) => BlocProvider.of(context);
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoadingState());
+
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold(
+      (error) =>
+          emit(FeaturedBooksFailureState(errorMessage: error.errorMessage)),
+      (value) => emit(FeaturedBooksSuccessState(books: value)),
+    );
+  }
 }
